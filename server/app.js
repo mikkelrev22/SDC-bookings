29b808f1d85ca0f443c5201735db7c752c9c37f9
Michael Thompson <michael.220488@gmail.com>
2019-08-06 10:07:55 -0700

    stress testing

--- server/app.js
+++ server/app.js
@@ -1,63 +1,62 @@
 const express = require('express');
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const bodyParse = require('body-parser');
 const app = express();
-
+const { Pool } = require('pg')
 const PORT = 3000;
-
+const password = require('./secret.js')
 app.use("/:id", express.static(__dirname + "/../client/public"));
-
-
-
 app.use(bodyParse.json());
 
-const connection = mysql.createConnection({
-    user: "root",
-    password: '123',
-    database: "bookings"
+const pool = new Pool({
+    host: "localhost",
+    database: "postgres",
+    username: "mikkelrev",
+    password: password.secret,
+    port: 5432,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log("Connected to MySQL DB");
-});
+pool.on('error', () => {
+    process.exit(-1);
+  });
+// connection.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log("Connected to MySQL DB");
+// });
 
 app.get('/getrentaldata/:id', (req, res) => {
    //this routes the request which comes from the 
    //getDataForId function in index.jsx and then passes
    //the id along to query the database and return the entire
    //data body to the client
-
-   connection.query(
+    
+   pool.query(
        `SELECT * FROM rental_price_info WHERE id=${req.params.id}`,
-       (err, results, fields) => {
+       (err, results) => {
            if (err) {
                console.log(`error with getting all fields by id from rental_price_info between server and db`, err);
            }
-           res.send(results);
-        
+           res.send(results)
        }
-   );
-    
+   );  
 })
 
-
 app.get('/getavailabilitydata/:id', (req, res) => {
-    connection.query(
+    pool.query(
         `SELECT * FROM rental_availability WHERE rental_id=${req.params.id}`,
-        (err, results, fields) => {
+        (err, results) => {
             if (err) {
                 console.log(`err getting data from rental_availability between server and db`, err);
             }
-            res.send(results);
-
-
+            res.send(results)
         }
     )
-
+// pool.end()
 })
-
 
 app.listen(PORT, ()=>{console.log(`Listening on ${PORT}`)});
